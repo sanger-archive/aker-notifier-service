2b391a6d4a4b4484d6525a03f0dedf7c70fd373b
Philip Jansen van Vuuren <pj5@sanger.ac.uk>
2018-03-20 17:04:53 +0000

    Update link to work plan

--- tests/message_tests.py
+++ tests/message_tests.py
@@ -8,22 +8,26 @@ from notifier import Message
 
 class MessageTests(unittest.TestCase):
 
-    FakeMessage = namedtuple('FakeMessage', 'event_type timestamp user_identifier metadata')
+    FakeMessage = namedtuple('FakeMessage',
+                             'event_type timestamp user_identifier metadata notifier_info')
     _fake_message = FakeMessage(event_type=consts.EVENT_SUB_CREATED,
                                 timestamp=datetime.now().isoformat(),
                                 user_identifier='test@sanger.ac.uk',
-                                metadata={'sample_custodian': 'sc@sanger.ac.uk'})
+                                metadata={'sample_custodian': 'sc@sanger.ac.uk'},
+                                notifier_info={'work_plan_id': 1})
 
     def test_init(self):
         message = Message(event_type=self._fake_message.event_type,
                           timestamp=dateutil.parser.parse(self._fake_message.timestamp),
                           user_identifier=self._fake_message.user_identifier,
-                          metadata=self._fake_message.metadata)
+                          metadata=self._fake_message.metadata,
+                          notifier_info=self._fake_message.notifier_info)
 
         self.assertEqual(message.event_type, self._fake_message.event_type)
         self.assertEqual(message.timestamp.isoformat(), self._fake_message.timestamp)
         self.assertEqual(message.user_identifier, self._fake_message.user_identifier)
         self.assertEqual(message.metadata, self._fake_message.metadata)
+        self.assertEqual(message.notifier_info, self._fake_message.notifier_info)
 
     def test_from_json(self):
         message_as_json = '''
@@ -33,15 +37,21 @@ class MessageTests(unittest.TestCase):
                 "user_identifier":"{}",
                 "metadata":{{
                     "{}":"{}"
+                }},
+                "notifier_info":{{
+                    "{}":{}
                 }}
             }}'''.format(self._fake_message.event_type,
                          self._fake_message.timestamp,
                          self._fake_message.user_identifier,
                          list(self._fake_message.metadata.keys())[0],
-                         self._fake_message.metadata['sample_custodian'])
+                         self._fake_message.metadata['sample_custodian'],
+                         list(self._fake_message.notifier_info.keys())[0],
+                         self._fake_message.notifier_info['work_plan_id'])
 
         message = Message.from_json(message_as_json)
         self.assertEqual(message.event_type, self._fake_message.event_type)
         self.assertEqual(message.timestamp.isoformat(), self._fake_message.timestamp)
         self.assertEqual(message.user_identifier, self._fake_message.user_identifier)
         self.assertEqual(message.metadata, self._fake_message.metadata)
+        self.assertEqual(message.notifier_info, self._fake_message.notifier_info)
